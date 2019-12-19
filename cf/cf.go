103713d8845ad7518372eba9ddd0de686517c707
Jakub Gawron <kubatek94@gmail.com>
2019-12-19 00:11:31 +0000

    Fetching zones and records, plus filtering works.

---
+++ cf/cf.go
@@ -0,0 +1,141 @@
+package cf
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"regexp"
+	"time"
+)
+
+const baseURL = "https://api.cloudflare.com/client/v4/"
+
+var httpClient = &http.Client{Timeout: 10 * time.Second}
+
+type Client struct {
+	header http.Header
+	zones []string
+}
+
+type DNSRecord struct {
+	ID string `json:"id"`
+	Name string `json:"name"`
+	IP string `json:"content"`
+	Zone string `json:"zone_id"`
+}
+
+func NewClient(email, key string) (*Client, error) {
+	client := &Client{
+		header: http.Header{
+			"Content-Type": {"application/json"},
+			"X-Auth-Email": {email},
+			"X-Auth-Key": {key},
+		},
+	}
+
+	r, err := client.get("zones?status=active&per_page=50")
+	if err != nil {
+		return nil, err
+	}
+
+	var data map[string]interface{}
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		return nil, err
+	}
+
+	if result, ok := data["result"]; ok {
+		result := result.([]interface {})
+		zones := make([]string, 0, len(result))
+
+		for _, zone := range result {
+			zone := zone.(map[string]interface{})
+			zones = append(zones, zone["id"].(string))
+		}
+
+		client.zones = zones
+	}
+
+	if client.zones == nil || len(client.zones) == 0 {
+		return nil, fmt.Errorf("no zones found")
+	}
+
+	return client, nil
+}
+
+func (cf *Client) FindDNSRecords(pattern *regexp.Regexp) ([]DNSRecord, error) {
+	allRecords := make([]DNSRecord, 0)
+
+	for _, zone := range cf.zones {
+		records, err := cf.fetchRecords(zone, pattern)
+		if err != nil {
+			return allRecords, err
+		}
+		allRecords = append(allRecords, records...)
+	}
+
+	return allRecords, nil
+}
+
+func (cf *Client) UpdateDNSRecord(record DNSRecord, ip string) error {
+	return fmt.Errorf("updates not implemented yet")
+}
+
+func (cf *Client) get(path string) (*http.Response, error) {
+	r, err := http.NewRequest("GET", baseURL + path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	r.Header = cf.header
+	return httpClient.Do(r)
+}
+
+func decodeRecords(body io.Reader) ([]DNSRecord, error)  {
+	decoder := json.NewDecoder(body)
+
+	var err error
+	var token json.Token
+
+	// skip until we get to "result" array
+	for decoder.More() && token != "result" {
+		token, err = decoder.Token()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// decode each object inside array into slice of DNSRecord struct
+	results := make([]DNSRecord, 0)
+	if err = decoder.Decode(&results); err != nil {
+		return nil, err
+	}
+
+	return results, nil
+}
+
+func (cf *Client) fetchRecords(zone string, pattern *regexp.Regexp) ([]DNSRecord, error) {
+	r, err := cf.get(fmt.Sprintf("zones/%s/dns_records", zone))
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+
+	records, err := decodeRecords(r.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if pattern == nil {
+		return records, nil
+	}
+
+	matchedRecords := make([]DNSRecord, 0, len(records))
+	for _, record := range records {
+		if pattern.MatchString(record.Name) {
+			matchedRecords = append(matchedRecords, record)
+		}
+	}
+
+	return matchedRecords, nil
+}
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"github.com/kubatek94/dyndns-cloudflare-adapter/cf"
 	"log"
 	"net/http"
 	"os"
+	"regexp"
+	"strings"
 )
 
 func main() {
@@ -14,18 +17,59 @@ func main() {
 		log.Fatal("CF_API_EMAIL or CF_API_KEY missing from environment")
 	}
 
+	client, err := cf.NewClient(email, key)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	log.Println("Listening...")
-	if err := http.ListenAndServe(":8080", &updater{email, key}); err != nil {
+	if err := http.ListenAndServe(":8080", &updater{client}); err != nil {
 		log.Println(err)
 	}
 }
 
 type updater struct {
-	email, key string
+	*cf.Client
 }
 
 func (u *updater) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var err error
+	var records []cf.DNSRecord
+
 	q := r.URL.Query()
-	log.Println("Update DNS ", q.Get("hostname"), " to ", q.Get("myip"))
-	w.WriteHeader(500)
+	hostname := q.Get("hostname")
+	newIP := strings.TrimSpace(q.Get("myip"))
+	log.Println("Update DNS ", hostname, " to ", newIP)
+
+	if hostname != "" {
+		pattern, err := regexp.Compile(hostname)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "notfqdn", 400)
+			return
+		}
+
+		records, err = u.FindDNSRecords(pattern)
+	} else {
+		records, err = u.FindDNSRecords(nil)
+	}
+
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "nohost", 412)
+		return
+	}
+
+	for _, record := range records {
+		if record.IP != newIP {
+			if err = u.UpdateDNSRecord(record, newIP); err != nil {
+				log.Println(err)
+				http.Error(w, "dnserr", 500)
+				return
+			}
+		}
+	}
+
+	_, _ = w.Write([]byte("good"))
+	w.WriteHeader(200)
 }
